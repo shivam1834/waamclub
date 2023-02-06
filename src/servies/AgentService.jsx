44000caeb4714e38337d092c5ecf5a3cc74cfdbf
shivam1834 <123928566+shivam1834@users.noreply.github.com>
2023-02-06 16:23:16 +0530

    Update AgentService.jsx

--- src/servies/AgentService.jsx
+++ src/servies/AgentService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const CreateAgent = async (res) => {
-    const response = await axios.post('https://aviraspices.in/api/web/createAgent', res);
+    const response = await axios.post(''https://aviraspices.in/api/web/CreateAgent'', res);
     console.log(response.data,res)
     return response.data;
 }
